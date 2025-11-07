64b9f4945eba7a112c9aef63b14413a3124ff68d
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-11-07 15:37:10 -0800

    Basic React site

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav style={{ display: 'flex', justifyContent: 'center', gap: '10px', borderBottom: '2px solid black', background: 'black', padding: '20px' }}>
+        <nav style={{ display: 'flex', justifyContent: 'center', gap: '10px', borderBottom: '2px solid black', backgroundColor: 'black', padding: '20px' }}>
             <Link style={{ textDecoration: 'none', fontSize: '16px', fontFamily: 'sans-serif', color: 'white' }} to='/'>Home</Link>
             <Link style={{ textDecoration: 'none', fontSize: '16px', fontFamily: 'sans-serif', color: 'white' }} to='/about'>About</Link>
             <Link style={{ textDecoration: 'none', fontSize: '16px', fontFamily: 'sans-serif', color: 'white' }} to='/services'>Services</Link>
