63d0069ccff3c327166731215604efa847f7caf0
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-11-07 15:25:31 -0800

    Basic React site

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact'
+
+function App() {
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
