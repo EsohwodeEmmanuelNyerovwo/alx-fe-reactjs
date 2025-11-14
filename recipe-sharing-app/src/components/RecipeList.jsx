93dd3247c5915aa07b68a3d3f969017a2f270af0
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-11-14 12:31:04 -0800

    Recipe Sharing App

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,23 @@
 import useRecipeStore from "./recipeStore";
+import React from "react"
+import { Link } from "react-router-dom"
+import { useRecipeStore } from "./recipeStore"
+import SearchBar from "./SearchBar"
 
-function RecipesList() {
+
+export default function RecipeList() {
     const recipes = useRecipeStore((s) =>
         s.searchTerm ? s.filteredRecipes : s.recipes
-    );
+    )
+
 
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold">Recipes</h1>
 
-            {/* Search Bar */}
+
             <SearchBar />
+
 
             <ul className="mt-4 space-y-3">
                 {recipes.map((r) => (
@@ -23,8 +30,34 @@ function RecipesList() {
                 ))}
             </ul>
         </div>
-    );
+    )
 }
+
+// function RecipesList() {
+//     const recipes = useRecipeStore((s) =>
+//         s.searchTerm ? s.filteredRecipes : s.recipes
+//     );
+
+//     return (
+//         <div className="p-6">
+//             <h1 className="text-2xl font-bold">Recipes</h1>
+
+//             {/* Search Bar */}
+//             <SearchBar />
+
+//             <ul className="mt-4 space-y-3">
+//                 {recipes.map((r) => (
+//                     <li key={r.id} className="p-3 border rounded">
+//                         <Link to={`/recipes/${r.id}`} className="font-semibold">
+//                             {r.title}
+//                         </Link>
+//                         <p className="text-sm">{r.description}</p>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
 // import useRecipeStore from "./recipeStore";
 
 // const RecipeList = () => {
