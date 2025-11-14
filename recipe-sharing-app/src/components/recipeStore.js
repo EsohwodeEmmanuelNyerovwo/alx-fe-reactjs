d185b94fca41a8ea30dfe05aa15e445691063afc
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-11-14 11:52:33 -0800

    Recipe Sharing App

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,18 @@ import { create } from "zustand"
 const useRecipeStore = create(set => ({
     recipes: [],
     addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-    setRecipes: (recipes) => set({ recipes })
+    setRecipes: (recipes) => set({ recipes }),
+    // Update existing recipe by id
+    updateRecipe: (id, updates) =>
+        set((state) => ({
+            recipes: state.recipes.map((r) => (r.id === id ? { ...r, ...updates } : r)),
+        })),
+
+
+    // Delete recipe by id
+    deleteRecipe: (id) =>
+        set((state) => ({
+            recipes: state.recipes.filter((r) => r.id !== id),
+        })),
 }));
 export default useRecipeStore;
