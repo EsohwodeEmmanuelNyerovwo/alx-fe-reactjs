e40e4f97d8d0902795cb59ca4a140d7bcf6dead9
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-11-14 12:24:58 -0800

    Recipe Sharing App

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,27 @@
 import { create } from "zustand"
+import { nanoid } from 'nanoid'
 
 const useRecipeStore = create(set => ({
-    recipes: [],
-    addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-    setRecipes: (recipes) => set({ recipes }),
+    // recipes: [],
+    // addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+    // setRecipes: (recipes) => set({ recipes }),
+    searchTerm: '',
+    setSearchTerm: (term) => set({ searchTerm: term }),
+
+
+    filteredRecipes: [],
+    filterRecipes: () =>
+        set((state) => ({
+            filteredRecipes: state.recipes.filter((recipe) =>
+                recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+            ),
+        })),
+
+    // Add a new recipe
+    addRecipe: (recipe) =>
+        set((state) => ({
+            recipes: [...state.recipes, { id: nanoid(), ...recipe }],
+        })),
     // Update existing recipe by id
     updateRecipe: (id, updates) =>
         set((state) => ({
