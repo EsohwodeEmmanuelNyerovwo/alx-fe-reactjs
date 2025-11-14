ce62bec08c6852bf36c39fe4488de91223f1dce8
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-11-14 12:02:38 -0800

    Recipe Sharing App

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import { useRecipeStore } from '../store/recipeStore'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function DeleteRecipeButton({ recipeId, afterDelete }) {
     const deleteRecipe = useRecipeStore((s) => s.deleteRecipe)
+    const navigate = useNavigate()
 
 
     function handleDelete() {
         const ok = window.confirm('Are you sure you want to delete this recipe?')
         if (!ok) return
+
+
         deleteRecipe(recipeId)
-        if (typeof afterDelete === 'function') afterDelete()
+
+
+        if (typeof afterDelete === 'function') {
+            afterDelete()
+        } else {
+            navigate('/recipes')
+        }
     }
 
 
