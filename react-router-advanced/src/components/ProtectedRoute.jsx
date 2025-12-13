406f7e89c8bfedccf115d29a0cd0ef8beb7ca6a8
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-12-13 13:36:27 -0800

    react router

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-
-const isAuthenticated = () => localStorage.getItem('auth') === 'true';
+import { useAuth } from '../hooks/useAuth';
 
 const ProtectedRoute = ({ children }) => {
-    if (!isAuthenticated()) {
+    const { isAuthenticated } = useAuth();
+
+    if (!isAuthenticated) {
         return <Navigate to="/login" replace />;
     }
+
     return children;
 };
 
