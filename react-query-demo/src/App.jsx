d1c3c9ac3acbc8124e84d91281843d79211a817b
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-12-13 12:10:52 -0800

    Handling data with react query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
-import PostsComponent from './components/postsComponent';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PostsComponent from "./components/postsComponent";
+
 
 const queryClient = new QueryClient();
 
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <h1>React Query Demo</h1>
       <PostsComponent />
-    </>
+    </QueryClientProvider>
   );
 }
 
